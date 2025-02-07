8bf56a216e66f4c2cea8c400e2a3545835b5843a
Thamarai <tmanalan710@gmail.com>
2025-02-07 18:17:38 +0530

    the middleware was done

--- backEnd/src/middleware.ts
+++ backEnd/src/middleware.ts
@@ -1,24 +1,26 @@
-import {NextFunction,Request,Response} from 'express'
-import jwt, { JwtPayload } from 'jsonwebtoken';
-import {JWT_PASSWORD} from "./config"
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_PASSWORD } from "./config";
 
-export const userMiddleware = (req:Request,res:Response,next:NextFunction)=>{
-    const header = req.headers["authorization"]
-    const decoded = jwt.verify(header as string ,JWT_PASSWORD)
-    if(decoded){
-        if(typeof decoded === "string"){
-            res.status(200).json({
-                message:"You are logged in"
-            })
-            return;
-        }
-        // req.userId = (decoded as JwtPayload).id
-        next()
-
+export const userMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const header = req.headers["authorization"];
+  const decoded = jwt.verify(header as string, JWT_PASSWORD);
+  if (decoded) {
+    if (typeof decoded === "string") {
+      res.status(200).json({
+        message: "You are logged in",
+      });
+      return;
     }
-    else{
-        res.status(403).json({
-            message:"You are Not Logged In"
-        })
-    }
-}
+    req.userId = (decoded as JwtPayload).id;
+    next();
+  } else {
+    res.status(403).json({
+      message: "You are Not Logged In",
+    });
+  }
+};
